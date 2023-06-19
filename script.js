604d1e1278595c32be957942c3a31ada6c710087
isabrao <89057659+isabrao@users.noreply.github.com>
2023-06-19 17:20:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Lógica JavaScript aqui
-  // Exemplo: Manipulação do DOM ou eventos
-});
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Meu Site</title>
+  <style>
+    /* Estilos CSS aqui */
+  </style>
+</head>
+<body>
+  <header>
+    <!-- Cabeçalho aqui -->
+  </header>
+  
+  <nav>
+    <div class="dropdown">
+      <button class="dropbtn">Menu</button>
+      <div class="dropdown-content">
+        <a href="#" onclick="changeTab('inicio')">Início</a>
+        <a href="#" onclick="changeTab('imagens')">Imagens</a>
+        <a href="#" onclick="changeTab('grupo')">Grupo</a>
+      </div>
+    </div>
+  </nav>
+  
+  <div id="inicio" class="tab">
+    <!-- Conteúdo da seção de Início aqui -->
+  </div>
+  
+  <div id="imagens" class="tab">
+    <!-- Conteúdo da seção de Imagens aqui -->
+  </div>
+  
+  <div id="grupo" class="tab">
+    <!-- Conteúdo da seção de Grupo aqui -->
+  </div>
+
+  <footer>
+    <!-- Rodapé aqui -->
+  </footer>
+
+  <script>
+    // Função para alternar entre as abas
+    function changeTab(tabName) {
+      // Esconde todas as abas
+      var tabs = document.getElementsByClassName("tab");
+      for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+      }
+      
+      // Mostra a aba selecionada
+      document.getElementById(tabName).style.display = "block";
+    }
+  </script>
+</body>
+</html>
