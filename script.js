a8b1304bb3a89ae035e2f319b02184f3e25b6a96
isabrao <89057659+isabrao@users.noreply.github.com>
2023-06-19 18:29:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Meu Site</title>
-  <style>
-    /* Estilos CSS aqui */
-  </style>
-</head>
-<body>
-  <header>
-    <!-- Cabeçalho aqui -->
-  </header>
-  
-  <nav>
-    <div class="dropdown">
-      <button class="dropbtn">Menu</button>
-      <div class="dropdown-content">
-        <a href="#" onclick="changeTab('inicio')">Início</a>
-        <a href="#" onclick="changeTab('imagens')">Imagens</a>
-        <a href="#" onclick="changeTab('grupo')">Grupo</a>
-      </div>
-    </div>
-  </nav>
-  
-  <div id="inicio" class="tab">
-    <!-- Conteúdo da seção de Início aqui -->
-  </div>
-  
-  <div id="imagens" class="tab">
-    <!-- Conteúdo da seção de Imagens aqui -->
-  </div>
-  
-  <div id="grupo" class="tab">
-    <!-- Conteúdo da seção de Grupo aqui -->
-  </div>
+// Selecione todos os botões de expansão
+const expandButtons = document.querySelectorAll('.expand-button');
 
-  <footer>
-    <!-- Rodapé aqui -->
-  </footer>
+// Itere sobre cada botão
+expandButtons.forEach(button => {
+  // Adicione um ouvinte de evento de clique
+  button.addEventListener('click', () => {
+    // Encontre o elemento pai (image-caption) do botão atual
+    const imageCaption = button.parentNode;
+    // Encontre o elemento de descrição dentro do image-caption
+    const description = imageCaption.querySelector('.description-text');
 
-  <script>
-    // Função para alternar entre as abas
-    function changeTab(tabName) {
-      // Esconde todas as abas
-      var tabs = document.getElementsByClassName("tab");
-      for (var i = 0; i < tabs.length; i++) {
-        tabs[i].style.display = "none";
-      }
-      
-      // Mostra a aba selecionada
-      document.getElementById(tabName).style.display = "block";
-    }
-  </script>
-</body>
-</html>
+    // Verifique se a descrição está visível ou oculta
+    if (description.style.display === 'none') {
+      // Se est
