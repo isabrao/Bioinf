7160f2d2b23990eca0a3ec8e17ba19e7f5dd68d9
isabrao <89057659+isabrao@users.noreply.github.com>
2023-06-19 22:20:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const expandButtons = document.querySelectorAll(".expand-button");
 function expandContent() {
   const imageCaption = this.parentNode;
   const expanded = imageCaption.classList.contains("expanded");
-  
+
   if (expanded) {
     imageCaption.classList.remove("expanded");
   } else {
@@ -34,9 +34,24 @@ expandButtons.forEach((button) => {
 // Alternar exibição dos tópicos de conteúdo ao clicar nos três riscos
 const menuIcon = document.querySelector(".menu-icon");
 const topicsPopup = document.querySelector(".topics-popup");
+const topicLinks = document.querySelectorAll(".topics-popup li");
 
 function toggleTopicsPopup() {
   topicsPopup.classList.toggle("active");
 }
 
+function scrollToSection(e) {
+  e.preventDefault();
+  const targetSectionId = this.dataset.target;
+  const targetSection = document.querySelector(targetSectionId);
+  
+  if (targetSection) {
+    topicsPopup.classList.remove("active");
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
 menuIcon.addEventListener("click", toggleTopicsPopup);
+topicLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
