921bad3ec3ae00e916b4a33f95f74a8f410638c8
isabrao <89057659+isabrao@users.noreply.github.com>
2023-06-20 00:42:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-// Função para alternar a exibição da descrição ao clicar no botão de expansão
-function expandDescription() {
-  const expandButtons = document.querySelectorAll('.expand-button');
-  
-  expandButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const description = button.nextElementSibling;
-      description.style.display = description.style.display === 'none' ? 'block' : 'none';
-    });
-  });
-}
+// Código Java para manipulação de eventos e outras funcionalidades do site
 
-// Função para rolar para a seção correspondente quando um item do menu é clicado
-function scrollToSection() {
-  const menuItems = document.querySelectorAll('.menu-item');
-  
-  menuItems.forEach(item => {
-    item.addEventListener('click', (event) => {
-      event.preventDefault();
-      const sectionId = item.getAttribute('href');
-      const section = document.querySelector(sectionId);
-      window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth'
-      });
-    });
-  });
-}
+// Função para expandir/diminuir a descrição ao clicar no botão expand
+const expandButtons = document.querySelectorAll(".expand-icon");
 
-// Chamada das funções após o carregamento do DOM
-document.addEventListener('DOMContentLoaded', () => {
-  expandDescription();
-  scrollToSection();
+expandButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const description = button.nextElementSibling;
+    description.classList.toggle("expanded");
+  });
 });
