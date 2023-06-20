e1cb16a46165a1b70aa723e1769485276bf12f9e
isabrao <89057659+isabrao@users.noreply.github.com>
2023-06-19 23:38:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-// Animação das seções ao rolar para baixo
-const sections = document.querySelectorAll("section");
-const windowHeight = window.innerHeight;
-
-function animateSections() {
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    if (sectionTop < windowHeight) {
-      section.classList.add("animate-slide-up");
-    }
+// Adicionar evento de clique aos botões de expansão
+var expandButtons = document.querySelectorAll('.expand-button');
+expandButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var caption = this.parentNode;
+    caption.classList.toggle('show');
   });
-}
-
-window.addEventListener("scroll", animateSections);
-
-// Revelar conteúdo ao clicar no botão
-const expandButtons = document.querySelectorAll(".expand-button");
-
-function expandContent() {
-  const imageCaption = this.parentNode;
-  const expanded = imageCaption.classList.contains("expanded");
-
-  if (expanded) {
-    imageCaption.classList.remove("expanded");
-  } else {
-    imageCaption.classList.add("expanded");
-  }
-}
-
-expandButtons.forEach((button) => {
-  button.addEventListener("click", expandContent);
 });
 
-// Alternar exibição dos tópicos de conteúdo ao clicar nas duas linhas
-const menuIcon = document.querySelector(".menu-icon");
-const topicsPopup = document.querySelector(".topics-popup");
-
-function toggleTopicsPopup() {
-  topicsPopup.classList.toggle("active");
-}
-
-menuIcon.addEventListener("click", toggleTopicsPopup);
+// Adicionar evento de clique ao ícone do menu
+var menuIcon = document.querySelector('.menu-icon');
+var header = document.querySelector('header');
+menuIcon.addEventListener('click', function() {
+  this.classList.toggle('close');
+  header.classList.toggle('menu-open');
+});
